aa91700c01474a779580fd00b416863a7800908b
vander moleker <vandermoleker@gmail.com>
2024-12-03 07:47:05 -0800

    update

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get("/buy/:id",async (req,res)=>{
     const swapMarket=await getSwapMarket(targetToken);
     if(!swapMarket) return res.json({status:"error",error:"NO_MARKET"})
     // const result=await swapTokenRapid(targetToken,swapMarket.poolKeys,0.0001,false);
-    const result=await swapTokenFastestWallet(connection,wallet,targetToken,swapMarket.poolKeys,0.001,false)
+    const result=await swapTokenFastestWallet(connection,wallet,targetToken,swapMarket.poolKeys,0.1,false)
     return res.json({status:"success",data:result})
 });
 
