28767802173d971a65d59fd91f3df22d30f773aa
vander moleker <vandermoleker@gmail.com>
2025-01-08 12:20:57 -0800

    updat

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get("/pumpfun/buy/:id",async (req,res)=>{
     const targetToken=req.params.id;
     const password=req.headers.passkey;
     if(!password||(password!=process.env.PASSWORD)) return res.json({status:"error",error:"WRONG_PASSWORD"})
-    await pumpfunSwapTransaction(targetToken,0.1,true);
+    await pumpfunSwapTransaction(targetToken,0.3,true);
     // await pumpfunSwapTransactionFasterWallet(connection,wallet,targetToken,0.3,true) 
     return res.json({status:"success"})
 })
